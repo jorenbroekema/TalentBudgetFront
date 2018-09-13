03cc32d31450b53e53507efc1249bc61032d5ae5
Joren Broekema <Joren.Broekema@ing.com>
2018-09-13 08:52:24 +0200

    Merged branch 'master' into 'feat/dialogexpenditure'

---
+++ js/components/ExpenditureDialog.js
@@ -0,0 +1,19 @@
+import './Expenditure.js';
+class BudgetExpenditureDialog extends BudgetExpenditure {
+
+  connectedCallback(){
+    const shadow = this.attachShadow({ mode: 'open' });
+    const expenditureDialog = document.createElement('div');
+    const title = this.title;
+    const icon = this.icon;
+    const budget = this.budget;
+
+    expenditureDialog.classList.add('expenditure-dialog');
+
+    expenditureDialog.innerHTML = `
+      <style></style>
+    `;
+    shadow.appendChild(expenditureDialog);
+  }
+}
+customElements.define('budget-expenditure-dialog', BudgetExpenditureDialog);
